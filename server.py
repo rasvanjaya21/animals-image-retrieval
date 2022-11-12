f46b615482bbe48479925f4dcba2f8bd9766536f
rasvanjaya21 <mrsejati17@gmail.com>
2022-11-13 05:58:40 +0700

    same as before

--- server.py
+++ server.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask.helpers import send_from_directory
 from flask_cors import CORS, cross_origin
 
-app = Flask(__name__, static_folder='build/')
+app = Flask(__name__, static_folder="static/build", static_url_path="/")
 CORS(app)
 
-@app.route('/api')
+@app.route("/api", methods=["GET"])
 @cross_origin()
 def index():
     return {"predictions": ["0.5", "0.6", "0.7"]}
@@ -16,4 +16,4 @@ def serve():
     return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
