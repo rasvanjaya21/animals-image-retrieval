2a7a74d25df61829dbc90f67f58af6ba6d28fd22
rasvanjaya21 <mrsejati17@gmail.com>
2022-11-15 18:05:24 +0700

    remove any unused

--- server.py
+++ server.py
@@ -1,19 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask.helpers import send_from_directory
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__, static_folder="templates/", static_url_path="/")
 CORS(app)
-
-# @app.route("/api", methods=["GET"])
-# @cross_origin()
-# def index():
-#     return {"predictions": ["0.5", "0.6", "0.7"]}
-
-# @app.route("/favicon.ico")
-# @cross_origin()
-# def favicon():
-#     return send_from_directory(app.static_folder, 'favicon.ico')
 
 @app.route("/")
 @cross_origin()
