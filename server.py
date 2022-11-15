5cb775b7925043e400898c5275d4f14c1b9ee2c8
rasvanjaya21 <mrsejati17@gmail.com>
2022-11-15 17:55:43 +0700

    update server.py

--- server.py
+++ server.py
@@ -1,25 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask.helpers import send_from_directory
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__, static_folder="static/build", static_url_path="/")
 CORS(app)
 
-@app.route("/api", methods=["GET"])
-@cross_origin()
-def index():
-    return {"predictions": ["0.5", "0.6", "0.7"]}
+# @app.route("/api", methods=["GET"])
+# @cross_origin()
+# def index():
+#     return {"predictions": ["0.5", "0.6", "0.7"]}
 
-
-@app.route("/favicon.ico")
-@cross_origin()
-def favicon():
-    return send_from_directory(app.static_folder, 'favicon.ico')
+# @app.route("/favicon.ico")
+# @cross_origin()
+# def favicon():
+#     return send_from_directory(app.static_folder, 'favicon.ico')
 
 @app.route("/")
 @cross_origin()
-def serve():
-    return send_from_directory(app.static_folder, 'index.html')
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
